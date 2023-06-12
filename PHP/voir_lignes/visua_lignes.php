cae65868902c7da2e543a38bef741c3c42a2663a
Maxence <levesqu221@campus.unicaen.fr>
2023-06-12 13:46:32 +0200

    ajout des noms de ville

--- PHP/voir_lignes/visua_lignes.php
+++ PHP/voir_lignes/visua_lignes.php
@@ -6,17 +6,33 @@
         $cheminParent = dirname(__DIR__);
         include_once $cheminParent . '/pdo_agile.php';
         $conn = OuvrirConnexionPDO($dbOracle,$db_usernameOracle,$db_passwordOracle);
-        $sql = "Select * from vik_ligne";
-        $nbLignes = LireDonneesPDO1($conn,$sql,$tab);
 
-    
-        if($nbLignes == 0)
-                echo "erreur";
-            else{
-                for($i=0; $i<$nbLignes; $i++){
-                    echo "<option value='option$i'>",$tab[$i]["LIG_NUM"],"</option>";
-                }
+        $erreur = false;
+        $sqlLigParA = "select lig_num,c.com_nom as depart ,b.com_nom as arrivee from vik_ligne l
+                    join  vik_commune c on c.com_code_insee=l.com_code_insee_debu 
+                    join  vik_commune b on b.com_code_insee=l.com_code_insee_term
+                    where lig_num like '%A%'";
+        echo sqlLigParA;
+        $sqlLigParB ="select lig_num,c.com_nom as depart ,b.com_nom as arrivee from vik_ligne l
+                    join  vik_commune c on c.com_code_insee=l.com_code_insee_debu 
+                    join  vik_commune b on b.com_code_insee=l.com_code_insee_term
+                    where lig_num like '%B%' ";
+       
+        $nbLignes = LireDonneesPDO1($conn,$sqlLigParA,$tabA);
+        if($nbLignes == 0){
+            $erreur=true;
+        }
+        $nbLignes = LireDonneesPDO1($conn,$sqlLigParB,$tabB);
+        if($nbLignes == 0){
+            $erreur=true;      
+        }
+        if(!$erreur){
+            for($i=0; $i<$nbLignes; $i++){
+                echo "<option value='option$i'>",$tabA[$i]["LIG_NUM"]," ", $tabA[$i]["DEPART"]," - ",$tabA[$i]["ARRIVEE"],"</option>";
+                $y= i+1;
+                echo "<option value='option$y'>",$tabB[$i]["LIG_NUM"]," ", $tabB[$i]["DEPART"]," - ",$tabB[$i]["ARRIVEE"],"</option>";
             }
+        }
     }
 
     
