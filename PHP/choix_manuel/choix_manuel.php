0f077b68d417d9a0b4a137ba6f892515d06228ed
Alenso Lopes <alenso.lopes@gmail.com>
2023-06-12 23:02:14 +0200

    commencement modif reservation

---
+++ PHP/choix_manuel/choix_manuel.php
@@ -0,0 +1,20 @@
+<?php
+    function afficherVilleDebut($ligne) {
+        include_once '../pdo_agile.php';
+        include '../param_connexion_etu.php';
+
+        $conn = OuvrirConnexionPDO($dbOracle,$db_usernameOracle,$db_passwordOracle);
+        $errreur = false;
+
+        $sql = "select distinct com_nom from vik_commune join vik_noeud using(com_code_insee) where lig_num = '" . $ligne ."'";
+        $nbLigne =  LireDonneesPDO1($conn, $sql, $tab);
+        
+        if($nbLigne == 0)
+            $erreur = true;
+        if(!$erreur) {
+            for($i = 0; $i < $nbLigne; $i++) {
+                echo "<option value=''>".$tab[$i]["COM_NOM"]."</option>";
+            }
+        }
+    }
+?>
