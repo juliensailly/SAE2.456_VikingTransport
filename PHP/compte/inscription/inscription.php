e64b524ecb846b6c8776d14651704aa2e0fd65db
Alenso Lopes <alenso.lopes@gmail.com>
2023-06-12 15:17:35 +0200

    ajout du regex

--- PHP/compte/inscription/inscription.php
+++ PHP/compte/inscription/inscription.php
@@ -1,20 +1,43 @@
 <?php
-    $nb = 0;
-    for($i=0;$i<sizeof($_POST);$i++){
-        if(checkIfExist($_POST[$i])){
-            $nb++;
+    
+    if(checkIfExist($_POST) == -1){
+        echo "Veuillez renseigner tout les champs";
+    }else if(checkIfExist($_POST) == -2){
+        echo "Veuillez renseigner tout les champs avec des lettres ou des chiffres";
+    }else{
+        $nom = $_POST["nom"];
+        $prenom = $_POST["prenom"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $datenaiss = $_POST["datenaiss"];
+        include_once 'PHP\pdo_agile.php';
+        $conn = OuvrirConnexionPDO($dbOracle,$db_usernameOracle,$db_passwordOracle);
+        $sql = "INSERT INTO vik_client (CLI_NOM, CLI_PRENOM, CLI_COURRIEL, CLI_PASSWORD, CLI_DATENAISS) VALUES ('$nom', '$prenom', '$email', '$password', '$datenaiss')";
+        $nbLignes = majDonneesPDO($conn,$sql);
+        if($nbLignes == 0)
+            echo "erreur";
+        else{
+            echo "Inscription réussie";
         }
     }
 
-    if($nb == sizeof($_POST)){
-        echo "Veuillez remplir tous les champs";
-    }else{
-        echo "oui";
+    function checkIfExist($tab){
+        if(isset($tab["nom"]) && isset($tab["prenom"]) && isset($tab["email"]) 
+            && isset($tab["password"]) && isset($tab["datenaiss"])){
+                if(useRegex($tab["nom"]) && useRegex($tab["prenom"]) && useRegexEmail($tab["email"])) return 0;
+                else return -2; 
+        }
+        return -1;
     }
     
 
-    function checkIfExist($name){
-        if(empty($name)) return true;
-        return false;
+    function useRegex($input) {
+        $regex = '/^[a-zA-Z0-9_]+$/';
+        return preg_match($regex, $input);
+    }
+
+    function useRegexEmail($input){
+        $regex = '/^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.[a-zA-Z0-9_]+$/';
+        return preg_match($regex, $input);
     }
 ?>
