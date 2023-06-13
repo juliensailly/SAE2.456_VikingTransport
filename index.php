4680d0982c0f26cb1d669fc295c0ed7b16b4885d
waylow1 <max2742contact@gmail.com>
2023-06-14 01:03:41 +0200

    fin de soirée

--- index.php
+++ index.php
@@ -17,10 +17,8 @@
       <li><a href="PHP/horairesLignes/horaires_ligne.php">HORAIRES</a></li>
       <li><a href="PHP/compte/modifier_profil/showProfil.php">MODIFIER PROFIL</a></li>
       <li><a href="PHP/statistiques/statistiques.php">STATISTIQUES</a></li>
-      <li><a href="PHP/reservation_NInscrit_ligne_unique/index.php">RESERVER</a></li>
-      <li><a href="PHP/choix_manuel/trajet.php">CHOISIR MANUELLEMENT</a></li>
-      <li><a href="PHP/voir_reservation/voir_reservation.php">ADMIN VOIR RESERVATIONS</a></li>
-      <li><a href="PHP/client_inactif/client_inactif.php">ADMIN VOIR CLIENTS INACTIFS</a></li>
+      <li><a href="PHP/choix_manuel/trajet.php">RESERVER</a></li>
+      <li><a href="admin.php">MENU ADMINISTRATEUR</a></li>
     </ul>
   </nav>
 </body>
