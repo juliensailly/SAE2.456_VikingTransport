dee5e4f44283e959b588a2544279a7e1b1980039
Alenso Lopes <alenso.lopes@gmail.com>
2023-06-14 09:24:07 +0200

    modif mineure

--- index.php
+++ index.php
@@ -12,21 +12,28 @@
 <body>
 <nav>
     <ul>
-      <li><a href="PHP/compte/connexion/formulaire.html">CONNEXION</a></li>
-      <li><a href="PHP/compte/inscription/formulaire.html">INSCRIPTION</a></li>
-      <li><a href="PHP/horairesLignes/horaires_ligne.php">HORAIRES</a></li>
-      <li><a href="PHP/compte/modifier_profil/showProfil.php">MODIFIER PROFIL</a></li>
-      <li><a href="PHP/statistiques/statistiques.php">STATISTIQUES</a></li>
-      <li><a href="PHP/reservation_NInscrit_ligne_unique/index.php">RESERVER</a></li>
-      <li><a href="PHP/choix_manuel/trajet.php">CHOISIR MANUELLEMENT</a></li>
       <?php
         session_start();
+        if(!isset($_SESSION['email'])){
+          echo "<li><a href=\"PHP/compte/connexion/formulaire.html\">CONNEXION</a></li>";
+          echo "<li><a href=\"PHP/compte/inscription/formulaire.html\">INSCRIPTION</a></li>";
+        }
+      ?>
+      <li><a href="PHP/horairesLignes/horaires_ligne.php">HORAIRES</a></li>
+      <?php
+        if(isset($_SESSION['email']) && $_SESSION['email'] != null){
+          echo "<li><a href=\"PHP/compte/modifier_profil/form.html\">MODIFIER PROFIL</a></li>";
+        }
+      ?>
+      
+      <li><a href="PHP/statistiques/statistiques.php">STATISTIQUES</a></li>
+      <li><a href="PHP/choix_manuel/trajet.php">RESERVER</a></li>
+      <?php
         if(isset($_SESSION['admin'])){
           if($_SESSION['admin'] == true){
                     echo "<li><a href=\"admin.php\">ADMIN</a></li>";
             }
         }
-        
       ?>
       <?php
         if(isset($_SESSION['email'])){
